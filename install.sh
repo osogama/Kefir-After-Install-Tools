bf2f3944f8060e32cc9ade12f16afdd4cc426173
Alzemand <edilson_alz@hotmail.com>
2015-12-16 16:30:22 -0200

    Install Script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Kefir After Install on OS                                
+                                                                              
+#update
+sudo apt-get update
+
+#Instalar dependencias
+
+sudo apt-get install python-software-properties -y
+sudo apt-get install python-webkit -y
+
+#criar pastas e icons
+
+sudo mkdir /opt/kefir-after-install/
+sudo cp -R * /opt/kefir-after-install/
+sudo cp kefir-after-install.desktop /usr/share/applications/
+sudo chmod +x /usr/share/applications/kefir-after-install.desktop
+
+clear
+echo "A instalação foi concluída!"
+echo "Esse diretório pode ser deletado"
+echo "Localize em: Aplicativos > Ferramentas de Sistema > Kefir After Install"
+sleep 5
+
