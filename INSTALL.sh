f8fb7827804f0b0db171eb7983536253b8016a2b
Alzemand <edilson_alz@hotmail.com>
2015-12-20 16:29:18 -0200

    bug fix on installation

--- INSTALL.sh
+++ INSTALL.sh
@@ -16,10 +16,29 @@ sudo mkdir /opt/kefir-after-install/
 sudo cp -R * /opt/kefir-after-install/
 sudo cp kefir-after-install.desktop /usr/share/applications/
 sudo chmod +x /usr/share/applications/kefir-after-install.desktop
+sudo chmod +x /opt/kefir-after-install/*.*
 
 clear
 echo "A instalação foi concluída!"
 echo "Esse diretório pode pode ser deletado"
-echo "Aplicativos > Ferramentas de Sistema > Kefir After Install"
-sleep 5
+echo "Acesse em: Aplicativos > Ferramentas de Sistema > Kefir After Install"
+sleep 1
+
+#TELA DE OPÇÔES PARA USUARIO {{{
+echo
+echo "__________________________________________________"
+echo "EXECUTAR O PROGRAMA AGORA? / RUN PROGRAM NOW?[Y/N]"
+read -p "ENTER: " esc
+    case $esc in
+    Y|y|s|S) #ESCOLHA
+        cd  /opt/kefir-after-install/
+        gksudo python kefir-after-install.py
+        exit
+    
+    N|n
+        exit    
+    ;;
+esac
+
+
 
